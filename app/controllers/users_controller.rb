e76e713c00ed359e5592afefcea6c2101497b4a3
SevlaMare <sevilamare@gmail.com>
2020-05-13 19:33:06 -0300

    fix spelling on validation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,17 @@ class UsersController < ApplicationController
   def new; end
 
   def create
-    fail
+    # fail
+    @user = User.new(username: 
+      params[:username],
+      email: params[:email],
+      password: params[:password]
+    )
+
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new
+    end
   end
 end
