f0065d41eab46800f447b8c87a59e5f68be9760c
SevlaMare <sevilamare@gmail.com>
2020-05-14 10:20:00 -0300

    linter fix2

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,17 @@
 class User < ApplicationRecord
-  validates :username,
-    presence: true,
-    length: { in: 4..16 },
-    uniqueness: true
+  validates 'username',
+            presence: true,
+            length: { in: 4..16 },
+            uniqueness: true
 
   valid_email = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+/
-  validates :email,
-    presence: true,
-    length: { maximum: 120 },
-    uniqueness: { case_sensitive: false },
-    format: { with: valid_email }
+  validates 'email',
+            presence: true,
+            length: { maximum: 120 },
+            uniqueness: { case_sensitive: false },
+            format: { with: valid_email }
 
-  validates :password,
-    presence: true,
-    length: { in: 6..22 }
+  validates 'password',
+            presence: true,
+            length: { in: 6..22 }
 end
