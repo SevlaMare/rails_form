b9bd50c91c55ac527e6aad3ed57ba9ab66f33964
SevlaMare <sevilamare@gmail.com>
2020-05-13 22:40:29 -0300

    raw submit form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,21 @@
 class UsersController < ApplicationController
-  def new; end
+  def new
+    @user = User.new
+  end
 
   def create
     # fail
-    @user = User.new(username: 
-      params[:username],
-      email: params[:email],
-      password: params[:password]
+    @user = User.new(
+      username: params[:user][:username],
+      email: params[:user][:email],
+      password: params[:user][:password]
     )
 
     if @user.save
+      puts 'save successful'
       redirect_to new_user_path
     else
+      puts 'is not saved'
       render :new
     end
   end
