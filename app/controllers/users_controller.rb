33ee45a42cd5ddc98c8ec4fc8204e54ca6d6da93
SevlaMare <sevilamare@gmail.com>
2020-05-14 16:05:22 -0300

    linter fix1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,7 @@ class UsersController < ApplicationController
   end
 
   private
+
   def user_params
     params.permit(:username, :email, :password)
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,17 @@
 class User < ApplicationRecord
-
   validates :username,
-    presence: true,
-    length: { in: 4..16 },
-    uniqueness: true
+            presence: true,
+            length: { in: 4..16 },
+            uniqueness: true
 
   valid_email = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+/
   validates :email,
-    presence: true,
-    length: { maximum: 120 },
-    uniqueness: { case_sensitive: false },
-    format: { with: valid_email }
+            presence: true,
+            length: { maximum: 120 },
+            uniqueness: { case_sensitive: false },
+            format: { with: valid_email }
 
   validates :password,
-    presence: true,
-    length: { in: 6..22 }
+            presence: true,
+            length: { in: 6..22 }
 end
